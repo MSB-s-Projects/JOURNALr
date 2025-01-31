c84dde0569111c5dd3fea9f7400dcdeb38ef339c
Manobal-Singh-Bagady <manobalsinghbagady.bt21ele@pec.edu.in>
2025-01-31 22:27:24 +0530

    initialize JOURNALr project with Spring Boot and Maven setup

---
+++ src/main/java/com/msb/journalr/JournaLrApplication.java
@@ -0,0 +1,13 @@
+package com.msb.journalr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournaLrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournaLrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msb/journalr/JournaLrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msb.journalr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournaLrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
